b3cb0582cbb0f3b5b4d5217341e9df3a25f442cd
jeli01 <jeli01@naver.com>
2025-05-18 13:04:37 +0900

    chore: 설정 세팅

---
+++ src/main/java/io/github/jeli01/stompkafkapractice/StompKafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.github.jeli01.stompkafkapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompKafkaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompKafkaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jeli01/stompkafkapractice/StompKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeli01.stompkafkapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompKafkaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
